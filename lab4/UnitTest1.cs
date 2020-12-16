6ef57f3ec4ba6c6017fd9488ace62b980880f0bc
diko_ua <dlink.uaua@gmail.com>
2020-12-16 17:44:45 +0200

    initial commit

---
+++ lab4/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace lab4
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
